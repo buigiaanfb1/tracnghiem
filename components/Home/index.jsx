e2021960dc5a3e6a9e6d2f986ff0799fa6802240
Bui Gia An <buigiaanfb1@gmail.com>
2021-10-09 01:47:30 +0700

    [Add] MUI

---
+++ components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Div } from './styles';
+import ToggleColorMode from '../test';
+
+const Home = () => {
+  return (
+    <Div>
+      <ToggleColorMode />
+    </Div>
+  );
+};
+
+export default Home;
---
+++ components/Home/styles.js
@@ -0,0 +1,8 @@
+import { styled } from '@mui/system';
+
+export const Div = styled('div')({
+  color: 'darkslategray',
+  backgroundColor: 'aliceblue',
+  padding: 8,
+  borderRadius: 4,
+});
