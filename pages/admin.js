b96b25cfd77a3c171473a470e062272466cef803
Bui Gia An <buigiaanfb1@gmail.com>
2021-10-28 13:36:12 +0700

    [Layout] Admin Template

---
+++ pages/admin.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout/Layout';
+import Dashboard from '../components/admin/Dashboard';
+
+const Index = () => {
+  return <Dashboard />;
+};
+
+// Index.getLayout = (page) => <Layout title="History | Easy10">{page}</Layout>;
+
+export default Index;
